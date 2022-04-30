516f0f812e69297eb38699cd91e725b2cf7e2412
汉广 <serfend@foxmail.com>
2022-04-30 20:02:53 +0800

    feat[practice]completed status modify

--- src/views/problems/Practice/Train/ProblemList/index.vue
+++ src/views/problems/Practice/Train/ProblemList/index.vue
@@ -1,13 +1,9 @@
 <template>
-  <div>
-    <transition-group
-      name="slide-fade"
-      tag="ul"
-      class="slide-container"
-      @enter="enter"
-      @before-enter="beforeEnter"
-    >
-      <Problem v-for="(d,index) in filtered_data" v-show="!d.completed" :key="d.id" :data="d" :index="index" v-bind="$props" :completed.sync="d.completed" />
+  <div style="min-height:50rem">
+    <transition-group name="slide-fade" mode="out-in" tag="ul" class="slide-container" @enter="enter" @before-enter="beforeEnter">
+      <li v-for="(d,index) in filtered_data" v-show="!d.completed" :key="d.id" class="slide-fade-item">
+        <Problem :data="d" :index="index" v-bind="$props" :completed.sync="d.completed" />
+      </li>
     </transition-group>
   </div>
 </template>
@@ -22,22 +18,35 @@ export default {
   props: {
     data: { type: Array, default: () => [] }
   },
+  data: () => ({
+    filtered_data: null
+  }),
   computed: {
-    options() {
+    options () {
       const d = this.$store.state.problems.current_info
       return d && d.train_info
     },
-    filtered_data() {
-      const d = this.data || []
-      return d.map(i => Object.assign({ id: i.content, completed: false }, i))
+
+  },
+  watch: {
+    data: {
+      handler (val) {
+        let d = this.data || []
+        d = d.map(i => {
+          const r = Object.assign({ id: i.content, completed: false }, i)
+          return r
+        })
+        this.filtered_data = d
+      },
+      immediate: true
     }
   },
   methods: {
-    beforeEnter(el) {
+    beforeEnter (el) {
       el.style.opacity = 0
       el.style['margin-left'] = '5rem'
     },
-    enter(el, done) {
+    enter (el, done) {
       const delay = el.dataset.index * 150
       setTimeout(() => {
         Velocity(
@@ -50,3 +59,19 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.slide-fade-item {
+  list-style-type: none;
+  transition: all ease 1s;
+  font-size: 1.5rem;
+  opacity: 1;
+}
+.slide-fade-enter, .slide-fade-leave-to{
+  opacity: 0;
+  transform: translateX(100%);
+}
+.slide-fade-leave-active {
+  position: absolute;
+}
+
+</style>
