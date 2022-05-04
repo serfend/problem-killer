e19e037049a9cc9d9c06568e45a077bfbccc3ab8
汉广 <serfend@foxmail.com>
2022-05-04 21:17:32 +0800

    refactor[practice]hot key tip update

--- src/views/problems/Practice/index.vue
+++ src/views/problems/Practice/index.vue
@@ -5,7 +5,7 @@
       <div v-if="!showSelector" key="1">
         <span>
           <el-button type="danger" style="margin-bottom:0.5rem" @click="requireStart()">返回</el-button>
-          <span style="font-weight:600">【Ctrl+Shift+↑】键返回上一题，【Ctrl+Shift+↓】键进入下一题，题目内按【Ctrl+Alt+Enter】提交答案，【Ctrl+Alt+数字】选中选项</span>
+          <span style="font-size:0.8rem;font-weight:600">【Ctrl+Shift+↑】键返回上一题，【Ctrl+Shift+↓】键进入下一题，题目内按【Ctrl+Alt+Enter】提交答案，【Ctrl+Alt+数字】选中选项，提交正确后【Enter】结束该题。</span>
         </span>
         <Train :name="database" />
       </div>
