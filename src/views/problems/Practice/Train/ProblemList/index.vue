c5ec26b1f48f929186f57645151eedc9f57c914f
汉广 <serfend@foxmail.com>
2022-05-05 10:04:37 +0800

    fix[practice]show problem under combo_kill

--- src/views/problems/Practice/Train/ProblemList/index.vue
+++ src/views/problems/Practice/Train/ProblemList/index.vue
@@ -191,7 +191,7 @@ export default {
       const dic = this.current_problems
       let r = problems.slice(problem_range_start, problem_range_end || problems.length)
       if (shuffle_problem) r = shuffle(r)
-      if (combo_problem) r = r.filter(i => (dic[i.id] && dic[i.id].combo_kill) || combo_problem > 0)
+      if (combo_problem) r = r.filter(i => ((dic[i.id] && dic[i.id].combo_kill) || 0) < combo_problem)
       if (new_problem) r = r.filter(i => !(dic[i.id] && dic[i.id].total))
       return r
     },
