6e1647610538098c92b379104f7d1ef61785a218
汉广 <serfend@foxmail.com>
2022-05-07 16:56:36 +0800

    feat[practice-tip]check current mode

--- src/views/problems/Practice/index.vue
+++ src/views/problems/Practice/index.vue
@@ -5,7 +5,15 @@
       <div v-if="!showSelector" key="1">
         <span>
           <el-button type="danger" style="margin-bottom:0.5rem" @click="requireStart()">返回</el-button>
-          <span style="font-size:0.8rem;font-weight:600">【Ctrl+Shift+↑】键返回上一题，【Ctrl+Shift+↓】键进入下一题，题目内按【Ctrl+Alt+Enter】提交答案，【Ctrl+Alt+数字】选中选项，提交正确后【Enter】结束该题。</span>
+          <el-button type="text" @click="show_tip=!show_tip">{{ show_tip?'隐藏帮助':'查看帮助' }}</el-button>
+          <span v-if="show_tip" style="font-size:0.8rem;font-weight:600">
+            <p>【Ctrl+Shift+↑】键返回上一题，【Ctrl+Shift+↓】键进入下一题</p>
+            <p>题目内按【Ctrl+Alt+Enter】提交答案</p>
+            <p>【Ctrl+Alt+数字】选中选项</p>
+            <p>提交正确后【Enter】结束该题</p>
+            <p>双击题目以聚焦到该题</p>
+            <p>只会显示当前聚焦的题目和该题附近的题目</p>
+          </span>
         </span>
         <Train :name="database" />
       </div>
@@ -22,7 +30,8 @@ export default {
   },
   data: () => ({
     database: null,
-    showSelector: true
+    showSelector: true,
+    show_tip: false
   }),
   methods: {
     requireStart (v) {
