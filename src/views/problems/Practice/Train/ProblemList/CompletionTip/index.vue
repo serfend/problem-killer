6b3a914687f070592fe95953be04b114841f092f
汉广 <serfend@foxmail.com>
2022-05-10 09:25:35 +0800

    refactor[practice]split completion component

---
+++ src/views/problems/Practice/Train/ProblemList/CompletionTip/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div v-if="show" key="tip" class="slide-fade-item" style="text-align:center">
+    <el-button type="text" @click="updatedb({})">已完成本轮练习，再来一轮吧~</el-button>
+    <div>
+      <el-button
+        type="text"
+        :disabled="!Object.keys(wrong_current).length"
+        @click="$emit('requireRestProblem', { db: wrong_current })"
+      >复习本轮错题
+      </el-button>
+      <el-button
+        type="text"
+        :disabled="!Object.keys(wrong_history).length"
+        @click="$emit('requireRestProblem', { db: wrong_history })"
+      >再刷历史错题
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompletionTip',
+  data: () => ({
+    status: {
+      total: 0,
+      solved: 0,
+      wrong: 0
+    },
+    wrong_history: {},
+    wrong_current: {}
+  }),
+  computed: {
+    show () {
+      return true
+    },
+    current_problems () {
+      return this.$store.state.problems.current_problems
+    },
+    options () {
+      return this.$store.state.problems.current_options
+    },
+  },
+  watch: {
+    status: {
+      handler (v) {
+        this.$emit('onStatus', v)
+      },
+      immediate: true,
+      deep: true
+    }
+  },
+  methods: {
+    updatedb ({ db, is_manual = true }) {
+      this.$emit('requireRestProblem', { is_manual, db })
+    },
+    init_status (d) {
+      const status = {
+        total: d.length,
+        solved: 0,
+        wrong: 0
+      }
+      this.status = status
+    },
+    init_wrong_set (d) {
+      this.wrong_current = {}
+      const dict = {}
+      const { current_problems } = this
+      d.map(i => {
+        const item = current_problems[i.id]
+        if (item && item.wrong) dict[i.id] = item.combo_kill
+      })
+      this.wrong_history = dict
+    },
+    update_status ({ problem, is_right }) {
+      const { status } = this
+      status.solved++
+      if (!is_right) {
+        status.wrong++
+        this.wrong_current[problem.id] = true
+      }
+    }
+  }
+}
+</script>
