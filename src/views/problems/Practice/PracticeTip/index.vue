0a38667a9b2126025319fdee324bbe40d680efd0
汉广 <serfend@foxmail.com>
2022-05-14 19:48:55 +0800

    refactor[train-tip]split out

---
+++ src/views/problems/Practice/PracticeTip/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <span>
+    <el-button type="text" @click="$emit('update:showTip',!showTip)">{{ showTip ? '隐藏帮助' : '查看帮助' }}</el-button>
+    <span v-if="showTip" style="font-size:0.8rem;font-weight:600">
+      <p>【Ctrl+Shift+↑】键返回上一题，【Ctrl+Shift+↓】键进入下一题</p>
+      <p>题目内按【Ctrl+Alt+Enter】提交答案</p>
+      <p>【Ctrl+Alt+数字】选中选项</p>
+      <p>提交正确后【Enter】结束该题</p>
+      <p>双击题目以聚焦到该题</p>
+      <p>只会显示当前聚焦的题目和该题附近的题目</p>
+    </span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'PracticeTip',
+  props: {
+    showTip: { type: Boolean, default: false }
+  }
+}
+</script>
--- src/views/problems/Practice/index.vue
+++ src/views/problems/Practice/index.vue
@@ -6,15 +6,7 @@
         <el-card class="operation-bar">
           <el-button type="danger" style="margin-bottom:0.5rem" @click="requireStart({ is_manual: true })">返回
           </el-button>
-          <el-button type="text" @click="show_tip = !show_tip">{{ show_tip ? '隐藏帮助' : '查看帮助' }}</el-button>
-          <span v-if="show_tip" style="font-size:0.8rem;font-weight:600">
-            <p>【Ctrl+Shift+↑】键返回上一题，【Ctrl+Shift+↓】键进入下一题</p>
-            <p>题目内按【Ctrl+Alt+Enter】提交答案</p>
-            <p>【Ctrl+Alt+数字】选中选项</p>
-            <p>提交正确后【Enter】结束该题</p>
-            <p>双击题目以聚焦到该题</p>
-            <p>只会显示当前聚焦的题目和该题附近的题目</p>
-          </span>
+          <PracticeTip :show-tip.sync="show_tip" />
         </el-card>
         <Train :name="database" />
       </div>
@@ -27,7 +19,8 @@ export default {
   name: 'Practice',
   components: {
     DataBaseSelector: () => import('./DataBaseSelector'),
-    Train: () => import('./Train')
+    Train: () => import('./Train'),
+    PracticeTip: () => import('./PracticeTip')
   },
   data: () => ({
     database: null,
