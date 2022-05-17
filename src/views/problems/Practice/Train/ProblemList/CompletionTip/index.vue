037777f0816d6199fa64a07375853e6205025ea3
汉广 <serfend@foxmail.com>
2022-05-17 09:41:44 +0800

    feat[completion-tip]show problem-set-count

--- src/views/problems/Practice/Train/ProblemList/CompletionTip/index.vue
+++ src/views/problems/Practice/Train/ProblemList/CompletionTip/index.vue
@@ -3,16 +3,12 @@
     <el-button type="text" @click="updatedb({ is_manual: true })">已完成本轮练习，再来一轮吧~</el-button>
     <div>
       <el-button
+        v-for="i in actions"
+        :key="i.alias"
         type="text"
-        :disabled="!Object.keys(wrong_current).length"
-        @click="updatedb({ dict: wrong_current, is_manual: true })"
-      >复习本轮错题
-      </el-button>
-      <el-button
-        type="text"
-        :disabled="!Object.keys(wrong_history).length"
-        @click="updatedb({ dict: wrong_history, is_manual: true })"
-      >再刷历史错题
+        :disabled="!Object.keys(action_record[i.target]).length"
+        @click="updatedb({ dict: action_record[i.target], is_manual: true })"
+      >{{ i.alias }}({{ Object.keys(action_record[i.target]).length }}道)
       </el-button>
     </div>
   </div>
@@ -28,8 +24,14 @@ export default {
       solved: 0,
       wrong: 0
     },
-    wrong_history: {},
-    wrong_current: {}
+    action_record: {
+      wrong_history: {},
+      wrong_current: {},
+    },
+    actions: [
+      { alias: '复习本轮错题', target: 'wrong_current' },
+      { alias: '复习历史错题', target: 'wrong_history' },
+    ]
   }),
   computed: {
     show () {
@@ -67,21 +69,21 @@ export default {
     },
     init_wrong_set (d) {
       this.inited = true
-      this.wrong_current = {}
+      this.action_record.wrong_current = {}
       const dict = {}
       const { current_problems } = this
       d.map(i => {
         const item = current_problems[i.id]
         if (item && item.wrong) dict[i.id] = item.combo_kill
       })
-      this.wrong_history = dict
+      this.action_record.wrong_history = dict
     },
     update_status ({ problem, is_right }) {
       const { status } = this
       status.solved++
       if (!is_right) {
         status.wrong++
-        this.wrong_current[problem.id] = true
+        this.action_record.wrong_current[problem.id] = true
       }
     }
   }
