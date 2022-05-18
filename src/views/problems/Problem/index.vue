cf295041e0077230983e844536bd90269bd6648f
汉广 <serfend@foxmail.com>
2022-05-18 11:29:05 +0800

    fix[practice]empty check should ret undefined

--- src/views/problems/Problem/index.vue
+++ src/views/problems/Problem/index.vue
@@ -65,7 +65,7 @@ export default {
       this.$emit('requireFocus')
     },
     async onSubmit ({ is_right, is_manual, answer }) {
-      const is_empty_answer = !answer || (tCheck(answer) === tArray && (!answer.length || answer.find(i => i) === null))
+      const is_empty_answer = !answer || (tCheck(answer) === tArray && (!answer.length || answer.find(i => i) === undefined))
       if (is_empty_answer && !is_manual) {
         const result = await this.$confirm('确定要不填答案就提交吗').catch(e => { })
         if (result !== 'confirm') return
